ca4d92f7faaa46e903e424c2f7d4df8a5e4233ce
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-20 10:48:29 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class hello{
 public static void main(String[] args){
-System.out.println("Helo, world ..we are running in jenkins");
+System.out.println("Helo, world ..we are running in jenkins h h i hi");
 }
 }
